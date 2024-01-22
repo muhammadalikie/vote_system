5647cb45fbbc13f9c5d7bf2e5456022611b903c4
muhammadalikie <muhammadalikie@gmail.com>
2024-01-22 17:50:47 +0330

    synce project

--- vote/models.py
+++ vote/models.py
@@ -1,24 +1,29 @@
 from collections.abc import Iterable
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
 
 
 class VoteCart(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
-    
+
     def __str__(self) -> str:
         return self.name
+
 
 class Representative(models.Model):
     student = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     vote_cart = models.ForeignKey(VoteCart, on_delete=models.CASCADE)
+
     def __str__(self) -> str:
         return str(self.student.username)
+
     class Meta:
         unique_together = ['student', 'vote_cart']
+
 
 class Vote(models.Model):
     vote_cart = models.ForeignKey(VoteCart, on_delete=models.CASCADE)
@@ -30,11 +35,12 @@ class Vote(models.Model):
 
     class Meta:
         unique_together = ['student', 'representative', 'vote_cart']
-    
+
     def clean(self):
-        from django.core.exceptions import ValidationError
+        if self.vote_cart.id != self.student.vote_code:
+            raise ValidationError('Cant vote to others vote code!')
         if self.vote_cart != self.representative.vote_cart:
-            raise ValidationError('Cant vote to another votecart')
+            raise ValidationError('Cant vote to others votecart')
 
     def __str__(self) -> str:
         return str(self.student.username) + '->' + \
