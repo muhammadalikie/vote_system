54bacba89e100f5ac485413ee0eb52c009699955
muhammadalikie <muhammadalikie@gmail.com>
2024-07-13 00:07:25 +0330

    add retrive to permission of reguler user

--- vote/permissions.py
+++ vote/permissions.py
@@ -3,8 +3,8 @@ from rest_framework import permissions
 class UserPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if view.action == 'list':
+        if view.action in ['list', 'retrieve']:
             return bool(request.user and request.user.is_authenticated)
             
-        elif view.action in ['create', 'retrieve', 'update', 'partial_update', 'destroy']:
+        elif view.action in ['create', 'update', 'partial_update', 'destroy']:
             return bool(request.user and request.user.is_staff)
